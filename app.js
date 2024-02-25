787cc0add80c2d090d27d09c59030f77f1815fc2
Pedro Alcântara <pedro.filipi.alcantara@gmail.com>
2024-02-25 13:05:11 -0300

    raw node

--- app.js
+++ app.js
@@ -1,2 +1,36 @@
-import http from "http";
-// const http = require("http");
+// import http from "http";
+const http = require("http");
+const fs = require("fs");
+
+// event driven architecture
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Enter message</title></head>");
+    res.write(
+      `<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>`
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+});
+
+server.listen(3000);
